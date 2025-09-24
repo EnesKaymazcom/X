821854d6ecbabd5e1de4d2f44ddaf318f2d5a28e
Enes Kaymaz <contact@eneskaymaz.com>
2025-09-24 16:52:21 +0300

    Clean repository - only Vector X project files

--- Etsy Helper kopyası/peugeot.py
+++
@@ -1,9 +0,0 @@
-import binascii
-
-dosya_yolu = '/Volumes/ETSY/rcc_update/container.iso.bin'
-
-with open(dosya_yolu, 'rb') as dosya:
-    veri = dosya.read()
-
-hex_veri = binascii.hexlify(veri)
-print(hex_veri)
--- nesdesign/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"byyCVyH85ak062b5OxRvViTD28fPd3qFe27w35cXOWw=\"\n}"
